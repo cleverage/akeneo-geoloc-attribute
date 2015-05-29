c6266b3e8c285b8678650968534ab0d1650065dd
Arnaud Langlade <alanglade@clever-age.com>
2015-05-29 14:28:04 +0200

    Services has been moved to another directory

--- src/DependencyInjection/ClerverAgeGelocAttributeExtension.php
+++ src/DependencyInjection/ClerverAgeGelocAttributeExtension.php
@@ -24,7 +24,7 @@ class ClerverAgeGelocAttributeExtension extends Extension implements PrependExte
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/service'));
         $loader->load('attribute.yml');
         $loader->load('model.yml');
     }
